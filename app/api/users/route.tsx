d918bff256aece0a4c7e35a4b8c80b7bc6580aee
daco-aray <daco.aray@gmail.com>
2025-02-22 15:32:36 -0600

    feat: Updating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,17 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client"
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Mosh'},
-        { id: 2, name: 'Josh'}
-    ])
+export async function GET(request: NextRequest) {
+
+    const users = await prisma.user.findMany()
+
+    return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
     const validation = schema.safeParse(body)
-    if(!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400})
-    return NextResponse.json({id: 1, name: body.name}, { status: 201})
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 })
+
+    // Check if email is already taken
+    const user = await prisma.user.findUnique({
+        where: { email: body.email}
+    })
+
+    if (user)
+        return NextResponse.json({ error: 'User already exists'}, { status: 400})
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    })
+    return NextResponse.json(newUser, { status: 201 })
 }
